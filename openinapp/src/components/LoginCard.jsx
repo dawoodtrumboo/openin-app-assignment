ffbdc472c175002d36a1076d7e52f3f84c7e854b
Dawood Trumboo <107537057+dawoodtrumboo@users.noreply.github.com>
2023-09-15 14:44:41 +0530

    commit

--- openinapp/src/components/LoginCard.jsx
+++ openinapp/src/components/LoginCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import CustomButton from './CustomButton'
 
-const LoginCard = () => {
+const LoginCard = ({handleClick}) => {
 
   const style = {
     width: '100%',        
@@ -13,8 +13,8 @@ const LoginCard = () => {
   return (
     <div className='w-full '>
 
-        <div className='bg-white rounded-xl p-10 space-y-3 '>
-            <div className='flex flex-col space-y-2 font-lato'>
+        <div className='bg-white rounded-xl lg:text-[16px] p-10 space-y-3 lg:space-y-6  '>
+            <div className='flex flex-col space-y-2 lg:space-y-6  font-lato'>
             <span>Email Address</span>
             <input type='text' placeholder='Enter your email' className='bg-[#F5F5F5] px-3' style={style}/>
             </div>
@@ -26,6 +26,7 @@ const LoginCard = () => {
             <CustomButton 
             style ={style}
             name = {`Sign In`}
+            handleClick={handleClick}
             />
 
         </div>
